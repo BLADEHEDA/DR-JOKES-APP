4e050f96cb694bf85aceeced5763b81f1243fd71
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-15 23:14:32 +0100

    initial changes done

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faBars} from '@fortawesome/free-solid-svg-icons'
+import {faFaceAngry,faRetweet} from '@fortawesome/free-solid-svg-icons'
+
+
+
+
 
 const Footer = () => {
   return (
@@ -10,12 +14,12 @@ const Footer = () => {
       <ul className="links"> 
       <li className="link mb-[0.3em]">ABOUT</li>
       <li className="link">CONTACT</li>
-      <p className="icons mt-[1em]"> GET IN TOUCH</p>
-      <div className="icons"> 
-      <div className="icon"> </div>
-      <div className="icon"></div>
-      <div className="icon"></div>
-      <div className="icon"></div>
+      <p className="icons mt-[1em] text-xl"> GET IN TOUCH</p>
+      <div className="icons flex  justify-center mt-3"> 
+      <div className="icon"><FontAwesomeIcon icon={faFaceAngry} className="text-2xl mt-1"  /> </div>
+      <div className="icon"><FontAwesomeIcon icon={faRetweet} className="text-2xl mt-1"  /> </div>
+      <div className="icon"><FontAwesomeIcon icon={faFaceAngry} className="text-2xl mt-1"  /> </div>
+      <div className="icon"><FontAwesomeIcon icon={faRetweet} className="text-2xl mt-1"  /> </div>
       </div>
       </ul>
     </div>
