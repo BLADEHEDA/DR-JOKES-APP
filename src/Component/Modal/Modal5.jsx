52d333ba14f209f3a313059e987d3fe8ed9b76d7
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-02-20 18:08:45 +0100

    changes made

--- src/Component/Modal/Modal5.jsx
+++ src/Component/Modal/Modal5.jsx
@@ -10,7 +10,7 @@ const Modal5 = ({showModals5, setShowModals5}) => {
              <div className="modal-head-section flex justify-between px-5 mb-3">
              <h3 className="text-2xl md:text-3xl font-semibold"> JOkes Category </h3>
                    <button className='text-2xl font-semibold text-red-500 "'
-                    onClick={() =>{ alert("it is modal5")
+                    onClick={() =>{ 
                        setShowModals5()}}
                   >  X
                    </button>
