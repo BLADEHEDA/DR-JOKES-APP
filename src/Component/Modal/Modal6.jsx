a6ca137b8929e0d523477d02d83b0fb215eed78d
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-03-06 20:12:57 +0100

    changes done

--- src/Component/Modal/Modal6.jsx
+++ src/Component/Modal/Modal6.jsx
@@ -32,7 +32,7 @@ return(
 } 
   return (
     <section className="modal-section h-[60vh] px-[1em] w-[80%]  md:w-[60%] h-[85vh]  m-auto inset-0 z-50 bg-white 
-    fixed  py-3 rounded-2xl overflow-scroll"> 
+    fixed  py-3 rounded-2xl overflow-y-auto"> 
     {/* <article className='header-div fixed w-[60%]  z-[9999] mt-[-10px]'>  */}
     <div className="modal-head-section flex justify-between px-5 mb-3">
              <h3 className="text-2xl md:text-3xl font-semibold  "> Romantic Jokes</h3>
